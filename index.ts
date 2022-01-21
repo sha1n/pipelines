a4078ca13095d21253023c6ad012ede197f2149b
Shai Nagar <sha1n@users.noreply.github.com>
2022-01-21 21:52:06 +0200

    exported missing types from main (#16)

--- index.ts
+++ index.ts
@@ -9,7 +9,10 @@ export {
 } from './lib/types';
 export { NonRecoverablePipelineError } from './lib/errors';
 export { TransitionRecord, TransitionResolver, Terminal } from './lib/spi/types';
+export { InMemoryStateRepository, Identifiable } from './lib/spi/InMemoryStateRepository';
+export { NoopStateRepository } from './lib/spi/NoopStateRepository';
 export { createPipelineBuilder } from './lib/PipelineBuilder';
+export { PipelineDriver } from './lib/PipelineDriver';
 export { Pipeline } from './lib/Pipeline';
 export { createTransitionResolverBuilder } from './lib/spi/StaticTransitionResolverBuilder';
 export { StaticTransitionResolver } from './lib/spi/StaticTransitionResolver';
