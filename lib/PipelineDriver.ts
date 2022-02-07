0cb3cfc2fba71ec31a9ffcfd6f49db1e8f437588
sha1n <sha1n@users.noreply.github.com>
2022-02-08 00:11:20 +0200

    removed redundant ctor param

--- lib/PipelineDriver.ts
+++ lib/PipelineDriver.ts
@@ -1,4 +1,4 @@
-import { RetryPolicy, simpleRetryPolicy } from '@sha1n/about-time';
+import { RetryPolicy } from '@sha1n/about-time';
 import { Pipeline } from './Pipeline';
 import createPump from './createPump';
 import { HandlerContext, StatefulPipelineEntity } from './types';
@@ -6,7 +6,7 @@ import { HandlerContext, StatefulPipelineEntity } from './types';
 class PipelineDriver<T extends StatefulPipelineEntity<S>, S, C extends HandlerContext> {
   readonly push: (entity: T, ctx: C) => Promise<T>;
 
-  constructor(pipeline: Pipeline<T, S, C>, retryPolicy: RetryPolicy = simpleRetryPolicy(0, 0)) {
+  constructor(pipeline: Pipeline<T, S, C>, retryPolicy?: RetryPolicy) {
     this.push = createPump(pipeline, retryPolicy);
   }
 }
